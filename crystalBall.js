25d0f54bca3a1bad8bfc482a2382faee81e1b7fe
Anne-Mei Ong <annemeiong2@gmail.com>
2025-11-01 17:47:10 +0000

    try to make ball shake

--- crystalBall.js
+++ crystalBall.js
@@ -1,13 +1,8 @@
 function shake(){
     var ball = document.getElementById("ball")
-    var messageText = document.getElementById("message")
-
-    if(messageText != null){
-        messageText.parentNode.removeChild(messageText)
-
-    }
 
     ball.classList.add("shake")
 
     setTimeout(function(){ball.classList.remove("shake");},1000)
 }
+
