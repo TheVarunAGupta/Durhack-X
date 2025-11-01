2a21fbe1150ec13b3470087e821a106424280bf6
TheVarunAGupta <varunanshulgupta@gmail.com>
2025-11-01 18:31:48 +0000

    modified comparison engine to have correct key implementation

--- comparison_engine.py
+++ comparison_engine.py
@@ -1,8 +1,13 @@
 import random
 import os
+from dotenv import load_dotenv
 from google import genai
 
+load_dotenv()
+
 api_key = os.environ.get('GEMINI_API_KEY')
+if not api_key:
+    raise ValueError('Missing GEMINI_API_KEY in environment!')
 client = genai.Client(api_key=api_key)
 
 athletes = {
